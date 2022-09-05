6b50feb7166e77fb9f35318fd917ab625bdd4ce4
SmallEmoLover <missingtext@yandex.ru>
2022-09-06 01:12:35 +0300

    Refactor python job runner

--- server/core/check_solution.py
+++ server/core/check_solution.py
@@ -6,12 +6,9 @@ import pymongo
 from bson.objectid import ObjectId 
 
 def main():
-    check_id = sys.argv[1]
-    task_id = sys.argv[2]
-
-    sys.path.append(f'/tasks/{task_id}/')
-
-    working_dir = f'/checks/{check_id}'
+    check_id, task_id = sys.argv[1:3]
+    sys.path.append(os.path.join('/tasks', task_id))
+    working_dir = os.path.join('/checks', check_id)
     os.mkdir(working_dir)
     os.chdir(working_dir)
 
@@ -38,10 +35,10 @@ def main():
 
     database.checks.update_one({'_id': ObjectId(check_id)}, {'$set': { 'result': result, 'status': 'Проверено' }})
 
-def execute_file(filename):
+def execute_file(filename) -> bool:
     if filename.endswith('.py'):
         filename = filename[:-3]
-    module = importlib.import_module(f'{filename}')
+    module = importlib.import_module(filename)
     test = getattr(module, 'main')
     try:
         return test(sys.argv[3:])
